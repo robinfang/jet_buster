e11245e1cbcde81d50e916a8e4003a417f2f69ca
robinfang <thefmajia@gmail.com>
2020-01-08 17:45:16 +0800

    not finished

--- preprocess.py
+++ preprocess.py
@@ -5,6 +5,7 @@ import pdb
 import sys
 import gc
 import pickle
+import threading, queue
 import logging
 logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
     datefmt='%Y-%m-%d:%H:%M:%S',
@@ -15,7 +16,7 @@ def batch2rows(batch):
     # pdb.set_trace()
     rs = np.array([i[0:-1] for i in batch])
     # print(len(rs))
-    return np.array([jet_id, rs])
+    return pd.DataFrame([[jet_id, rs]])
 
 
 def p2j(particle_df):
@@ -32,15 +33,14 @@ def p2j(particle_df):
     category_list = [-2212, -2112, -321, -211, -13, -11, 11, 13, 22, 130, 211, 321, 2112, 2212]
     assert len_cate == len(category_list)
     particle_df["particle_category"] = particle_df["particle_category"].apply(category_list.index)
-    particle_df["particle_category"] = particle_df["particle_category"].apply(lambda x: to_categorical(x, num_classes=len(category_list), dtype=bool))
+    # particle_df["particle_category"] = particle_df["particle_category"].apply(lambda x: to_categorical(x, num_classes=len(category_list), dtype=np.uint8))
     logging.info(gc.collect())
     # print(t.head)
     # print(t.dtypes)
     # ["particle_category_{}".format(i) for i in range(14)]
     # 最多的喷注有115个粒子
     logging.info(sys.getsizeof(particle_df)/1024/1024)
-    particle_df = pd.concat([pd.DataFrame([list(x) for x in particle_df["particle_category"]], index=particle_df.index).add_prefix("g"), particle_df], axis=1)
-    # particle_df.drop(["particle_category"], inplace=True, axis=1)
+    # particle_df = pd.concat([pd.DataFrame([list(x) for x in particle_df["particle_category"]], index=particle_df.index).add_prefix("g"), particle_df], axis=1).drop(["particle_category"], axis=1)
     # logging.info("particle_category dropped")
     particle_df = particle_df.values
     logging.info(gc.collect())
@@ -53,21 +53,23 @@ def p2j(particle_df):
         # pdb.set_trace()
         if pid is None or pid != row[-1]:
             if current_batch is not None:
-                yield pd.DataFrame(batch2rows(current_batch))
+                result.append(batch2rows(current_batch))
             # 处理上一批的
             current_batch = [row]  # 新建本批次的
         else:
             current_batch.append(row)
         pid = row[-1]
         if idx == len_ds - 1:
-            yield pd.DataFrame(batch2rows(current_batch))
+            result.append(batch2rows(current_batch))
             # 到达最后一个元素
+    return pd.concat(result).reset_index(drop=True)
 
 
 def j2e(jet_df, grouped_particle_df):
     jet_df = jet_df.sort_values(["event_id", "jet_mass", "jet_energy"]).reset_index(drop=True)
     
     pass
+
 
 
 if __name__ == '__main__':
@@ -78,10 +80,20 @@ if __name__ == '__main__':
     logging.info(sys.getsizeof(particle_df)/1024/1024)
     particle_df = p2j(particle_df)
     logging.info(sys.getsizeof(particle_df)/1024/1024)
-    store = pd.HDFStore("data/{}.h5".format(test_or_train))
+
+    to_write = queue.Queue()
+
+    def writer():
+        # Call to_write.get() until it returns None
+        for k, df in iter(to_write.get, None):
+            df.to_hdf("data/{}.h5".format(test_or_train), "jet_id_{}".format(k), mode="a")
+    threading.Thread(target=writer).start()
+
     for item in particle_df:
-        store.append("data", item)
-    store.close()
+        to_write.put(item)
+    to_write.put(None)
+
+
     # with open("e:/data/{}_p.pickle".format(test_or_train), "wb") as f:
     #     pickle.dump(df, f, pickle.HIGHEST_PROTOCOL)
     # df.to_csv("e:/data/{}_particle_group_by_jet.csv".format(test_or_train), index=False)
